f1237c0d13cac3f69d14ce9fdf078c00e8833ac0
beigenut <beigenut@gmail.com>
2018-04-17 17:19:31 +0900

    Push js-snake-game file to repository

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -1,19 +1,25 @@
 import {ROWS, COLS} from './config';
 
+
+
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
+  // 맨 뒤 꼬리를 떼서 나가고자 하는 방향 앞으로 보내면 된다.
   this.joints = [
-    {x: 2, y: 0},
-    {x: 1, y: 0},
     {x: 0, y: 0},
+    {x: 1, y: 0},
+    {x: 2, y: 0}, // 머리
+//    {x: 3, y: 0}
   ];
 
   // 먹이의 좌표
-  this.fruit = {x: 3, y: 5};
+    // 먹이를 먹으면 꼬리 뒤로 보낸다.
+  this.fruit = {x: 10, y: 5};
 }
 
 SnakeGameLogic.prototype.up = function() {
   // 위쪽 화살표 키를 누르면 실행되는 함수
+  // 뱀이 오른쪽 : y 축 -1 
   console.log('up');
 }
 
@@ -28,8 +34,12 @@ SnakeGameLogic.prototype.left = function() {
 }
 
 SnakeGameLogic.prototype.right = function() {
-  // 오른쪽 화살표 키를 누르면 실행되는 함수
+
+  this.joints.push({x:this.joints[2].x+1, y:0});
+  this.joints.shift();
+//  this.xAix++;
   console.log('right');
+//  console.log('xArix')
 }
 
 SnakeGameLogic.prototype.nextState = function() {
@@ -37,6 +47,7 @@ SnakeGameLogic.prototype.nextState = function() {
   // 게임이 아직 끝나지 않았으면 `true`를 반환
   // 게임이 끝났으면 `false`를 반환
   console.log(`nextState`);
+//  console.log(this.joints);
   return true;
 }
 
