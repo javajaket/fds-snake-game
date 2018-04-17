41dee235cf87abc7af1160cf2de6fcf71e66cce6
Lee Chaewon <lchaewn@gmail.com>
2018-04-17 17:15:27 +0900

    right key

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -5,11 +5,9 @@ function SnakeGameLogic() {
   // 꼬리를 떼서 머리에 붙여준다
   // fruit[마지막인덱스]와 joints[0]이 같을 때 아이템 하나씩 추가
   this.joints = [
-    {x: 2, y: 5},
-    {x: 2, y: 4},
-    {x: 2, y: 3},
-    {x: 2, y: 2},
-    {x: 2, y: 1},
+    {x: 2, y: 0},
+    {x: 1, y: 0},
+    {x: 0, y: 0},
   ];
 
   // 먹이의 좌표. joints[0]과 같아 지면 랜덤 위치
@@ -34,7 +32,14 @@ SnakeGameLogic.prototype.left = function() {
 
 SnakeGameLogic.prototype.right = function() {
   // 오른쪽 화살표 키를 누르면 실행되는 함수. joints x++
+  // for (let i = 0; i < this.joints.length; i++) {
+  //   this.joints[i].x++;
+  // }
   console.log('right');
+  this.joints.pop();
+  this.joints.unshift({
+    x: this.joints[0].x + 1, 
+    y: 0}); // 꼬리를 떼서 머리 다음에 붙이기
 }
 
 SnakeGameLogic.prototype.nextState = function() {
@@ -43,7 +48,7 @@ SnakeGameLogic.prototype.nextState = function() {
   // 게임이 끝났으면 `false`를 반환.
 
   console.log(`nextState`);
-  return false;
+  return true;
 }
 
 export default SnakeGameLogic;
