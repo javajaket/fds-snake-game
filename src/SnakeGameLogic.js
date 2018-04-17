ac60acae585bdf16d3adb477ec10bbb854d094e4
Lee Chaewon <lchaewn@gmail.com>
2018-04-17 16:04:07 +0900

    주석 추가

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -1,43 +1,45 @@
 import {ROWS, COLS} from './config';
 
 function SnakeGameLogic() {
-  // 각 마디의 좌표를 저장하는 배열
+  // 각 마디의 좌표를 저장하는 배열. fruit[0]와 joints[0]이 같을 때 아이템 하나씩 추가
   this.joints = [
     {x: 2, y: 0},
     {x: 1, y: 0},
     {x: 0, y: 0},
   ];
 
-  // 먹이의 좌표
+  // 먹이의 좌표. joints[0]과 같아 지면 랜덤 위치
   this.fruit = {x: 3, y: 5};
 }
 
 SnakeGameLogic.prototype.up = function() {
-  // 위쪽 화살표 키를 누르면 실행되는 함수
+  // 위쪽 화살표 키를 누르면 실행되는 함수. joints y++
+  
   console.log('up');
 }
 
 SnakeGameLogic.prototype.down = function() {
-  // 아래쪽 화살표 키를 누르면 실행되는 함수
+  // 아래쪽 화살표 키를 누르면 실행되는 함수.joints y--
   console.log('down');
 }
 
 SnakeGameLogic.prototype.left = function() {
-  // 왼쪽 화살표 키를 누르면 실행되는 함수
+  // 왼쪽 화살표 키를 누르면 실행되는 함수. joints x--
   console.log('left');
 }
 
 SnakeGameLogic.prototype.right = function() {
-  // 오른쪽 화살표 키를 누르면 실행되는 함수
+  // 오른쪽 화살표 키를 누르면 실행되는 함수. joints x++
   console.log('right');
 }
 
 SnakeGameLogic.prototype.nextState = function() {
   // 한 번 움직여야 할 타이밍마다 실행되는 함수
-  // 게임이 아직 끝나지 않았으면 `true`를 반환
-  // 게임이 끝났으면 `false`를 반환
+  // 게임이 아직 끝나지 않았으면 `true`를 반환. 0부터 시작. x < 30, y < 20 일 때 true
+  // 게임이 끝났으면 `false`를 반환.
+
   console.log(`nextState`);
-  return true;
+  return false;
 }
 
 export default SnakeGameLogic;
