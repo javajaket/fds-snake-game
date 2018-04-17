aaf441ea4e8597b9a068020aac6e57e18c0f0d33
Lee Jaeyeon <1290jaeyeonlee@gmail.com>
2018-04-17 16:00:21 +0900

    주석 추가

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -1,15 +1,23 @@
 import {ROWS, COLS} from './config';
 
+// SnakeGameLogic 생성자 
 function SnakeGameLogic() {
   // 각 마디의 좌표를 저장하는 배열
+  
+  //뱀 마디의 좌표를 나타낸다.
+  // x가 0이면 가장 왼쪽이다.
+  // y가 0이면 가장 위쪽이다. 
+  // x와 y로 된 객체 한 쌍이 칸 한개를 나타낸다.
+  // 객체의 갯수가 게임 하단 현재 길이에 출력된다.
   this.joints = [
-    {x: 2, y: 0},
-    {x: 1, y: 0},
     {x: 0, y: 0},
+    {x: 1, y: 0},
+    {x: 2, y: 0},
   ];
+  
 
   // 먹이의 좌표
-  this.fruit = {x: 3, y: 5};
+  this.fruit = {x: 3, y: 3};
 }
 
 SnakeGameLogic.prototype.up = function() {
@@ -38,6 +46,8 @@ SnakeGameLogic.prototype.nextState = function() {
   // 게임이 끝났으면 `false`를 반환
   console.log(`nextState`);
   return true;
+  //return이 false이면 게임이 끝났다는 뜻이다.
+  //뱀의 색깔이 검정색이 된다.
 }
 
 export default SnakeGameLogic;
--- src/config.js
+++ src/config.js
@@ -2,7 +2,9 @@
 export const GAME_ROOT = '#game-root';
 
 // 게임 자동 실행 여부
-export const AUTO_START = true;
+export const AUTO_START = false;
+//true일 경우에는 nextState를 자동으로 해준다.
+//false일 경우에는 게임시작이라는 버튼이 생기고 버튼을 클릭해야 nextState가 실행된다.
 
 // 게임판의 행 개수
 export const ROWS = 20;
@@ -12,6 +14,8 @@ export const COLS = 30;
 
 // 뱀의 이동 주기 (밀리세컨드 단위)
 export const INITIAL_DELAY = 3000;
+//3초마다 nextState 주기가 바뀐다.
+//그때마다 이동시킨다.
 
 // 1초 마다 주기에 곱해줄 상수 (0.95로 지정하면 적당)
 export const DELAY_EXPONENT = 1;
