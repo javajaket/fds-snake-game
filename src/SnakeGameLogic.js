5a4ac15b73f25f408e4cf8e5542672dfb0b37033
Lee Chaewon <lchaewn@gmail.com>
2018-04-19 16:36:15 +0900

    먹이 좌표 랜덤 돌리기 망한 버전

--- src/SnakeGameLogic.js
+++ src/SnakeGameLogic.js
@@ -39,60 +39,56 @@ SnakeGameLogic.prototype.right = function() {
 
 SnakeGameLogic.prototype.nextState = function() {
   // 한 번 움직여야 할 타이밍마다 실행되는 함수
-  const newHead = {};
+  console.log(`nextState`);
+  const newHead = {x: this.joints[0].x, y: this.joints[0].y};
   // 뱀이 움직이는 방향
   if (this.direction === 'right') {
-    newHead.x = this.joints[0].x + 1, 
-    newHead.y = this.joints[0].y
-    // 자기 자신과 부딪히면 게임 종료
-    if (this.joints.some(item => item.x === newHead.x && item.y === newHead.y)) {
-      return false;
-    }
-    this.joints.unshift(newHead);
-    this.joints.pop();
+    newHead.x++;
   } else if (this.direction === 'left') {
-    newHead.x = this.joints[0].x - 1, 
-    newHead.y = this.joints[0].y
-    if (this.joints.some(item => item.x === newHead.x && item.y === newHead.y)) {
-      return false;
-    }
-    this.joints.unshift(newHead);
-    this.joints.pop();
-  } else if (this.direction === 'up') {
-    newHead.x = this.joints[0].x, 
-    newHead.y = this.joints[0].y - 1
-    if (this.joints.some(item => item.x === newHead.x && item.y === newHead.y)) {
-      return false;
-    }
-    this.joints.unshift(newHead);
-    this.joints.pop();
+    newHead.x--;
+  } else if (this.direction === 'up') { 
+    newHead.y--;
   } else if (this.direction === 'down') {
-    newHead.x = this.joints[0].x, 
-    newHead.y = this.joints[0].y + 1
-    if (this.joints.some(item => item.x === newHead.x && item.y === newHead.y)) {
-      return false;
-    }
-    this.joints.unshift(newHead);
-    this.joints.pop();
+    newHead.y++;
   } else {
     return true;
   }
-  // 먹이를 먹으면 1.먹이 밭 안에서 랜덤 위치 2.꼬리 하나 추가
-  if (this.fruit.x === newHead.x && this.fruit.y === newHead.y) {
-    this.fruit.x = Math.ceil(COLS * Math.random()); // 주사위 코드 const dice = Math.ceil(6 * Math.random());
-    this.fruit.y = Math.ceil(ROWS * Math.random());
-    this.joints.push({
-      x: newHead.x,
-      y: newHead.y
-    });
+  // 먹이를 먹으면 1.먹이 밭 안에서 랜덤 위치 2.꼬리(머리) 추가
+  if (this.fruit.x === this.joints[0].x && this.fruit.y === this.joints[0].y) {
+    function randomXY() {
+      while (true) {
+        const randomX = Math.ceil(COLS * Math.random());
+        const randomY = Math.ceil(ROWS * Math.random());
+        const readyFruit = {x:randomX, y:randomY}
+        for (let i = 0; i < this.joints.length; i++) {
+          if (this.joints[i] === readyFruit) {
+            continue;
+          }
+        }
+        return readyFruit;
+      }
+    }
+    const fruitXY = randomXY();
+    
+    this.fruit.x = fruitXY.x;
+    this.fruit.y = fruitXY.y;
+
+    this.joints.unshift(newHead);
+    return true;
   }
-  // 벽에 부딪히면 게임 종료
-  if (newHead.x >= COLS || newHead.y >= ROWS || newHead.x < 0 || newHead.y < 0) {
+  // 1.자기 자신과 부딪히거나 2.벽에 부딪히면 게임 종료
+  if (this.joints.some(item => item.x === newHead.x && item.y === newHead.y)) {
     // 게임이 끝났으면 `false`를 반환.
+    console.log('@@@@@@@@@@');
+    console.log(this.joints);
+    return false;
+  } else if (this.joints[0].x >= COLS || this.joints[0].y >= ROWS || this.joints[0].x < 0 || this.joints[0].y < 0) {
     return false;
   } else {
-    // 게임이 아직 끝나지 않았으면 `true`를 반환.
-    console.log(`nextState`);
+    // 게임이 아직 끝나지 않았으면 `true`를 반환
+    // 게임이 아직 끝나지 않았으면 계속 이동
+    this.joints.unshift(newHead);
+    this.joints.pop();
     return true;
   }
 }
